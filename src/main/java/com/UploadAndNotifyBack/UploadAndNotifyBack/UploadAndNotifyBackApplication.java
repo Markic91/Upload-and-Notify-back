b487df0020beae07249571210655febd4ab89eef
ANTIC MARKO <[antic.marko69@gmail.com](mailto:antic.marko69@gmail.com)>
2023-09-26 09:47:22 +0200

    coucou

---
+++ src/main/java/com/UploadAndNotifyBack/UploadAndNotifyBack/UploadAndNotifyBackApplication.java
@@ -0,0 +1,13 @@
+package com.UploadAndNotifyBack.UploadAndNotifyBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadAndNotifyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadAndNotifyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UploadAndNotifyBack/UploadAndNotifyBack/UploadAndNotifyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UploadAndNotifyBack.UploadAndNotifyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadAndNotifyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
