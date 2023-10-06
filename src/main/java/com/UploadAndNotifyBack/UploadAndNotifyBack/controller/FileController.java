8ebad5636a9ce234e35d874d82d235a657c69754
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2023-10-06 15:25:31 +0200

    ..

--- src/main/java/com/UploadAndNotifyBack/UploadAndNotifyBack/controller/FileController.java
+++ src/main/java/com/UploadAndNotifyBack/UploadAndNotifyBack/controller/FileController.java
@@ -18,6 +18,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Consumer;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class FileController {
@@ -38,15 +40,38 @@ public class FileController {
 
     @PostMapping("/files")
 
-    public ResponseEntity<String> createFiles(@RequestParam ("file") List<MultipartFile> files ) throws IOException {
-        {
-            for (MultipartFile file : files) {
-                System.out.println(file.getContentType());
-                System.out.println(file.getOriginalFilename());
-                System.out.println(file.getName());
+    public ResponseEntity<String> createFiles(@RequestParam ("file") List<MultipartFile> files,
+                                              @RequestParam ("url") List<String> urls,
+                                              @RequestParam ("exp") String exp,
+                                              @RequestParam (required = false,value ="mail") String mail ) {
 
+        ArrayList<File> myNewList = new ArrayList<>();
+        for (int i = 0; i < files.size(); i++) {
+            File myNewFile = new File();
+            myNewFile.setName(files.get(i).getOriginalFilename());
+            for (String url : urls) {
+                myNewFile.setLink(urls.get(i));
             }
+            myNewList.add(myNewFile);
         }
+
+        myNewList.forEach(item -> {item.setExpiration(exp);
+            switch (exp){
+                case "1 jour":
+                    item.setDate(LocalDateTime.now().plusDays(1));
+                    break;
+                case "1 mois":
+                    item.setDate(LocalDateTime.now().plusMonths(1));
+                    break;
+                case "1 an":
+                    item.setDate(LocalDateTime.now().plusYears(1));
+                    break;
+                case "jamais":
+                    break;
+            }});
+
+        myNewList.forEach(item -> item.setMail(mail));
+        fileRepository.saveAll(myNewList);
         return (new ResponseEntity<>("Successful", HttpStatus.OK));    }
     }
 //    public File createFile(@RequestBody File newFile) {
